e63b77b57d75c58a7c477ef4c2af05f53f38de98
ozneroL541 <l.21radice@gmail.com>
2026-04-17 19:47:10 +0200

    refactor: remove '|| true' from route addition in entrypoint scripts for vulnbox containers

--- project/network/vulnbox-1/entrypoint.sh
+++ project/network/vulnbox-1/entrypoint.sh
@@ -1,5 +1,4 @@
 #!/bin/bash
 set -e
-ip route add 172.16.2.0/24 via 172.16.1.2 || true
-ip route add 172.16.3.0/24 via 172.16.1.2 || true
-exec sleep infinity
+ip route add 172.16.2.0/24 via 172.16.1.2
+ip route add 172.16.3.0/24 via 172.16.1.2
--- project/network/vulnbox-3/entrypoint.sh
+++ project/network/vulnbox-3/entrypoint.sh
@@ -1,4 +1,3 @@
 #!/bin/bash
 set -e
-ip route add 172.16.3.0/24 via 172.16.2.4 || true
-exec sleep infinity
+ip route add 172.16.3.0/24 via 172.16.2.4
