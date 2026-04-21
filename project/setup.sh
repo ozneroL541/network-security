3772dd188e220af41203f403fecbb9e81f051f72
ozneroL541 <l.21radice@gmail.com>
2026-04-21 18:46:34 +0200

    feat: update Dockerfile and setup script for improved network configuration and permissions

--- project/setup.sh
+++ project/setup.sh
@@ -1,38 +1,34 @@
 #!/bin/bash
+# Start the vulnbox network
 sudo docker compose up --build --remove-orphans -d
-
+# Allow forwarding between containers
 sudo iptables -I FORWARD 1 -s 172.30.0.0/22 -d 172.30.0.0/22 -j ACCEPT
-
+# Remove the default IP addresses from the bridge interfaces
+sudo ip addr del 172.30.1.254/24 dev vulnlab1
+sudo ip addr del 172.30.2.254/24 dev vulnlab2
+sudo ip addr del 172.30.3.254/24 dev vulnlab3
 # Host
 # Tell your physical machine to send lab traffic to vulnbox1
 sudo ip route replace 172.30.1.0/24 via 172.30.0.2
 sudo ip route replace 172.30.2.0/24 via 172.30.0.2
 sudo ip route replace 172.30.3.0/24 via 172.30.0.2
-
 # vulnbox1
 sudo docker exec -u 0 vulnbox1 sysctl -w net.ipv4.ip_forward=1
-# Route to net2 and net3 via vulnbox2
 sudo docker exec -u 0 vulnbox1 ip route replace 172.30.2.0/24 via 172.30.1.2
 sudo docker exec -u 0 vulnbox1 ip route replace 172.30.3.0/24 via 172.30.1.2
 sudo docker exec -u 0 vulnbox1 ip route add default via 172.30.0.1
-
 # vulnbox2
 sudo docker exec -u 0 vulnbox2 sysctl -w net.ipv4.ip_forward=1
 sudo docker exec -u 0 vulnbox2 ip route replace 172.30.3.0/24 via 172.30.2.4
 sudo docker exec -u 0 vulnbox2 ip route add default via 172.30.1.1
-
 # vulnbox3
 sudo docker exec -u 0 vulnbox3 ip route add 172.30.3.0/24 via 172.30.2.4
 sudo docker exec -u 0 vulnbox3 ip route add default via 172.30.2.1
-
 # vulnbox4
 sudo docker exec -u 0 vulnbox4 ip route replace 172.30.3.0/24 via 172.30.2.4
 sudo docker exec -u 0 vulnbox4 ip route add default via 172.30.2.1
-
-
 # vulnbox5
 sudo docker exec -u 0 vulnbox5 sysctl -w net.ipv4.ip_forward=1
 sudo docker exec -u 0 vulnbox5 ip route add default via 172.30.2.1
-
 # vulnbox6
-#sudo docker exec -u 0 vulnbox6 ip route add default via 172.30.3.1
+sudo docker exec -u 0 vulnbox6 ip route add default via 172.30.3.1
